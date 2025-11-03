37ce64a10db5f14f2a435deafc82ee950b610073
korakrit lapachart <korakritxgod@gmail.com>
2025-11-04 02:18:14 +0700

    อัปเดตโค้ดล่าสุด

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/', // 👈 สำคัญ! เพื่อให้ router ทำงานตอน deploy
+})
